83ca30d08dc3f813776080a719e7c921b1a42dbd
d4rkd3vil <sreeraj.s.c.vfc@gmail.com>
2022-08-07 22:39:08 +0530

    first commit

--- chat-spammer.py
+++ chat-spammer.py
@@ -1,18 +1,11 @@
 import pyautogui
-from win10toast import ToastNotifier
 import time
 time.sleep(5)
 words=open('word.txt','r')
-notify=ToastNotifier()
-for i in range(6):
+for i in range(74):
     word=words.readline()
     pyautogui.typewrite(word)
     pyautogui.press('enter')
-notify.show_toast(
-    title='Spam Complete',
-    msg='The Script has been run Successfully',
-    #icon_path='icon.png',
-    duration=5
-    )   
+    time.sleep(1)
 words.close()
 print('Sussfully completed')
